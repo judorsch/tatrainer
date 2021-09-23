ccf769134e28d1ee3f938bb0e91ae8764f002f6e
judorsch <jdorsch@udel.edu>
2021-09-22 21:03:00 -0400

    reveal answer

--- src/Components/CardViewer.tsx
+++ src/Components/CardViewer.tsx
@@ -2,7 +2,7 @@ import {Col, Card as BootstrapCard} from 'react-bootstrap'
 import { Card } from '../interfaces/card'
 
 
-export function CardViewer({card}: {card:Card}): JSX.Element {
+export function CardViewer({card, answerRevealed}: {card:Card, answerRevealed:boolean}): JSX.Element {
     return( 
     <Col>
     <BootstrapCard>
@@ -11,9 +11,9 @@ export function CardViewer({card}: {card:Card}): JSX.Element {
             <BootstrapCard.Text>
             <strong>Prompt</strong>: {card.prompt}
             </BootstrapCard.Text>
-            <BootstrapCard.Text>
+            {answerRevealed && <BootstrapCard.Text>
             <strong>Answer</strong>: {card.answer}
-            </BootstrapCard.Text>
+            </BootstrapCard.Text>}
         </BootstrapCard.Body>
 
     </BootstrapCard>
--- src/Components/ControlPanel.tsx
+++ src/Components/ControlPanel.tsx
@@ -4,11 +4,14 @@ import CARDS from '../asset/cards.json'
 import { getRandomElement } from '../utilities/data';
 
 
-export function ControlPanel({setCard}: {setCard:(c: Card)=>void}): JSX.Element{
+export function ControlPanel({setCard, reveal, answerRevealed}: {setCard:(c: Card)=>void, reveal:(r:boolean)=>void, answerRevealed:boolean}): JSX.Element{
     function setRandomCard(){
         setCard(getRandomElement(CARDS as Card[]))
     }
-    return <Col>
-    <h1>Control Panel</h1> 
-    <Button onClick={setRandomCard}>Swap Current Card</Button></Col>
+    return( 
+    <Col>
+        <h1>Control Panel</h1> 
+        <Button onClick={setRandomCard}>Swap Current Card</Button>
+        <Button onClick={()=>reveal(!answerRevealed)}>Reveal Answer</Button>
+    </Col>)
 }
