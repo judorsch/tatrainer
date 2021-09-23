7d5ab8cb0eb0aef11ed11d27f97d3c65c41a394b
judorsch <jdorsch@udel.edu>
2021-09-22 21:07:04 -0400

    margins

--- src/Components/ControlPanel.tsx
+++ src/Components/ControlPanel.tsx
@@ -6,12 +6,13 @@ import { getRandomElement } from '../utilities/data';
 
 export function ControlPanel({setCard, reveal, answerRevealed}: {setCard:(c: Card)=>void, reveal:(r:boolean)=>void, answerRevealed:boolean}): JSX.Element{
     function setRandomCard(){
+        reveal(false);
         setCard(getRandomElement(CARDS as Card[]))
     }
     return( 
     <Col>
         <h1>Control Panel</h1> 
-        <Button onClick={setRandomCard}>Swap Current Card</Button>
-        <Button onClick={()=>reveal(!answerRevealed)}>Reveal Answer</Button>
+        <Button onClick={setRandomCard} className="m-4">Swap Current Card</Button>
+        <Button onClick={()=>reveal(!answerRevealed)} className="m-4">Reveal Answer</Button>
     </Col>)
 }
