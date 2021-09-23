3465ff64c603625085be222583b1ab600ab07877
judorsch <jdorsch@udel.edu>
2021-09-22 21:38:50 -0400

    suffle users

--- src/Components/UserList.tsx
+++ src/Components/UserList.tsx
@@ -1,12 +1,9 @@
 import EditableDndList, { Task } from "editable-dnd-list";
 import { useState } from "react";
 
-export function UserList():JSX.Element{
-    const [users, setUsers] = useState<Task[]>([
-        {id: '', text:"Dr. Bart"}
-    ]);
-    function handleChange(items: Task[]):void{
-        console.log(items);
+export function UserList({users, setUsers}: {users:Task[], setUsers:(t: Task[])=>void}):JSX.Element{
+    function handleChange(newUsers: Task[]):void{
+        console.log(newUsers);
     }
     return <EditableDndList items={users} onChange={handleChange}></EditableDndList>
 }
